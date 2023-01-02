a4e6b5c29353f0f24dc6f24ded71db3fecf702e1
nirutechhxxcj <121539165+nirutechhxxcj@users.noreply.github.com>
2023-01-02 16:21:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 DOWNLOAD_LOCATION = "./Downloads"
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5845400826:AAHil-Ow8XUY69-evPAj60eSeTqnz1zARnQ")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5836909294:AAFrPW2WTIC59UqV5tHAix4tzqaFOQC0wa8")
 # BOT_TOKEN = "5845400826:AAHil-Ow8XUY69-evPAj60eSeTqnz1zARnQ"  #  @aryan_vbot
 APP_ID = int(os.environ.get("API_ID", "952608"))
 API_HASH = os.environ.get("API_HASH", "8d8d0ad8e3d4bcd54420190f57da78ad")
